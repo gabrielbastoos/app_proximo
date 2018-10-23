6f2694e67ea08fcb5a506b860dc0064d936aaf4d
caroluchoa <43659990+caroluchoa@users.noreply.github.com>
2018-10-22 23:13:48 -0300

    incluida pagina dados e pagina fim

--- flask_app.py
+++ flask_app.py
@@ -31,7 +31,7 @@ def pedido():
 
 @app.route("/ingrediente", methods=['POST'])
 def ingrediente():
-    
+
     refeicao_escolhida = request.form['refeicao']
     bebida_escolhida = request.form['bebida']
 
@@ -40,17 +40,61 @@ def ingrediente():
 
 @app.route("/dados", methods=['POST'])
 def dados():
+        arroz = request.form.get('arroz')
+        if arroz:
+            arroz = "arroz -"
+        else:
+            arroz = ""
 
-    refeicao_escolhida = request.form['refeicao_escolhida']
-    bebida_escolhida = request.form['bebida_escolhida']
-    
-    return render_template('dados.html',refeicao_escolhida=refeicao_escolhida,bebida_escolhida=bebida_escolhida)
+        feijao = request.form.get('feijao')
+        if feijao:
+            feijao = "feijão -"
+        else:
+            feijao = ""
+
+        tomate = request.form.get('tomate')
+        if tomate:
+            tomate = "tomate -"
+        else:
+            tomate = ""
+
+        alface = request.form.get('alface')
+        if alface:
+            alface = "alface -"
+        else:
+            alface = ""
+
+        batatafrita = request.form.get('batatafrita')
+        if batatafrita:
+            batatafrita = "batata frita -"
+        else:
+            batatafrita = ""
+
+        ovofrito = request.form.get('ovofrito')
+        if ovofrito:
+            ovofrito = "ovo frito -"
+        else:
+            ovofrito = ""
+
+        farofa = request.form.get('farofa')
+        if farofa:
+            farofa = "farofa -"
+        else:
+            farofa = ""
+
+        cenoura = request.form.get('cenoura')
+        if cenoura:
+            cenoura = "cenoura -"
+        else:
+            cenoura = ""
+
+        return render_template('dados.html', arroz=arroz, feijao=feijao, tomate=tomate, alface=alface, batatafrita=batatafrita,cenoura=cenoura,ovofrito=ovofrito,farofa=farofa)
+
+@app.route("/fim", methods=['POST'])
+def fim():
+    return render_template('fim.html')
 
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", debug=True)
 
-    '''arq = open('lista.txt', 'w')
-    arq.write(registro)
-    arq.close()
-    '''
