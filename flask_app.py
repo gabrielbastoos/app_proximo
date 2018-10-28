6aea41d26135063ffecf06d9e6d43c0553b53f87
gabriel.bastoos <gabriel.bastos@poli.ufrj.br>
2018-10-27 21:00:24 -0300

    pagina de dados reestruturada

--- flask_app.py
+++ flask_app.py
@@ -43,21 +43,27 @@ def mostrarRefeicao(restaurante_id):
 	return render_template('refeicao.html', restaurante=restaurante, refeicao=refeicao)
 
 
-@app.route('/restaurantes/<int:restaurante_id>/<string:refeicao>/')
-@app.route('/restaurantes/<int:restaurante_id>/menu/<string:refeicao>/menu')
-def mostrarBebida(restaurante_id,refeicao):
+
+@app.route('/restaurantes/<int:restaurante_id>/<int:refeicao_id>/')
+@app.route('/restaurantes/<int:restaurante_id>/menu/<int:refeicao_id>/menu')
+def mostrarBebida(restaurante_id,refeicao_id):
     
     restaurante = session.query(Restaurante).filter_by(id=restaurante_id).one()
+    refeicao = session.query(Refeicao).filter_by(id=refeicao_id).one()
     bebida = session.query(Bebida).filter_by(restaurante_id=restaurante.id)
     return render_template('bebida.html', restaurante=restaurante, bebida=bebida,refeicao=refeicao)
 
-@app.route("/dados")
-def dados():
-    
-    refeicao_escolhida = request.form['refeicao_escolhida']
-    bebida_escolhida = request.form['bebida_escolhida']
 
-    return render_template('dados.html', refeicao_escolhida=refeicao_escolhida, bebida_escolhida=bebida_escolhida)
+
+@app.route('/restaurantes/<int:restaurante_id>/<int:refeicao_id>/<int:bebida_id>')
+@app.route('/restaurantes/<int:restaurante_id>/menu/<int:refeicao_id>/<int:bebida_id>/dados')
+def dados(restaurante_id,refeicao_id,bebida_id):
+
+    restaurante = session.query(Restaurante).filter_by(id=restaurante_id).one()
+    refeicao = session.query(Refeicao).filter_by(id=refeicao_id).one()
+    bebida = session.query(Bebida).filter_by(id=bebida_id).one()
+
+    return render_template('dados.html', restaurante=restaurante, refeicao_escolhida=refeicao, bebida_escolhida=bebida)
 
 '''@app.route("/pedido", methods=['POST'])
 def pedido():
@@ -65,14 +71,6 @@ def pedido():
     restaurante_escolhido = request.form['lista_restaurantes']
 
     return render_template('refeicao.html', restaurante_escolhido=restaurante_escolhido)
-
-@app.route("/ingrediente", methods=['POST'])
-def ingrediente():
-
-    refeicao_escolhida = request.form['refeicao']
-    bebida_escolhida = request.form['bebida']
-
-    return render_template('ingrediente.html', refeicao_escolhida=refeicao_escolhida,bebida_escolhida=bebida_escolhida)
 
 @app.route("/fim", methods=['POST'])
 def fim():
